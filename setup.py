46a88a2554b18bcb640ca05c83baaed847bb5419
rgao77 <gaoxiaotao58@gmail.com>
2023-05-01 00:31:11 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='montecarlo_simulator',
     version='0.1',
     description='A Monte Carlo Simulator for dice games',
-    url='https://github.com/your_username/your_repository',
+    url='https://github.com/rgao77/DS5100-2023-01-project0/tree/main/Montecarlo_simulator',
     author='Ran Gao',
     author_email='gr@example.com',
     license='MIT',

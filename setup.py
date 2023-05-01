8400c77a0827bd1f37316a5df0c4d87e10fba5c1
rgao77 <gaoxiaotao58@gmail.com>
2023-04-30 23:43:33 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='montecarlo_simulator',
+    version='0.1',
+    description='A Monte Carlo Simulator for dice games',
+    url='https://github.com/your_username/your_repository',
+    author='Ran Gao',
+    author_email='gr@example.com',
+    license='MIT',
+    packages=['Montecarlo_simulator'],
+    install_requires=[
+        'pandas',
+        'matplotlib',
+    ],
+)
